7a1f64bb2b44e0b4041edf44c5a956eea2d66bff
sayan047 <sayanghosh004@gmail.com>
2020-10-04 11:24:58 +0530

    Added solution for Vanya_and_Fence

--- A/Vanya_and_Fence/Vanya_and_Fence.cpp
+++ A/Vanya_and_Fence/Vanya_and_Fence.cpp
@@ -1,30 +1,43 @@
-/*  Coded by sidanand67  */
+/*  Coded by ghsayan  */
 /* Problem Link: https://codeforces.com/contest/677/problem/A */
 
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define OJ                            \
+	freopen("input.txt", "r", stdin); \
+	freopen("output.txt", "w", stdout);
+const int mod = 1'000'000'007;
+const int N = 3e5;
+int arr[N];
 
-
-int main() {
-	int numberOfFriends, heightOfFence;
-	cin>> numberOfFriends >> heightOfFence;
-
-	vector<int> heightOfPersons(numberOfFriends);
-	for (int i = 0; i < numberOfFriends; i++) {
-		cin>> heightOfPersons[i];
+void solve()
+{
+	int i, n, h, sum = 0, x;
+	cin >> n >> h;
+	fo(i, n)
+	{
+		cin >> x;
+		if (x > h)
+			sum += 2;
+		else
+			sum += 1;
 	}
+	cout << sum;
+}
 
-	int widthOfRoad = 0;
-	for (int i = 0; i < numberOfFriends; i++) {
-		if (heightOfPersons[i] > heightOfFence) {
-			widthOfRoad += 2;
-		}
-		else {
-			widthOfRoad++;
-		}
+int main()
+{
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	int t = 1;
+	// cin >> t;
+	while (t--)
+	{
+		solve();
 	}
-
-	cout<< widthOfRoad;
-
 	return 0;
 }
