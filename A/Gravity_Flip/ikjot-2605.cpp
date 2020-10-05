5513516360e67d7051fea8467b3d1066652ed4f2
Ikjot Singh <ikjotsd.191cs123@nitk.edu.in>
2020-10-05 20:05:25 +0530

    Added c++ solution for Gravity Flip

---
+++ A/Gravity_Flip/ikjot-2605.cpp
@@ -0,0 +1,17 @@
+//
+// Created by ikjot-2605 on 05/10/20.
+//
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
