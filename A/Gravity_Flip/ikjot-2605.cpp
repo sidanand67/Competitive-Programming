f10b8a68fd56f0fd4163a25f19992e35a02bdf45
Ikjot Singh <ikjotsd.191cs123@nitk.edu.in>
2020-10-05 21:42:10 +0530

    Added comments to the solution

--- A/Gravity_Flip/ikjot-2605.cpp
+++ A/Gravity_Flip/ikjot-2605.cpp
@@ -6,10 +6,14 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
+    //here arr represents input array of length of the n columns available
     int arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
+    //all we do is sort the input array
+    //because the final arrangement will be in ascending order
+    //therefore we sort it and then display the output
     sort(arr,arr+n);
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
