f9bdcdd15ffb0990c31d1cc9f8c9939af5ea6ad0
shivangigoel1302 <shivangigoel1301@gmail.com>
2020-10-05 14:20:56 +0530

    changes for petya and strings

--- A/Petya_and_strings/shivangigoel1302.cpp
+++ A/Petya_and_strings/shivangigoel1302.cpp
@@ -8,13 +8,18 @@
 using namespace std;
 int compare(string s1,string s2){
    for(int i = 0 ; i < s1.size(); i++){
+        //check whether ith char of string 1 is greater than string 2
+        //ascii difference must be equal to 32 for uppercase and lowercase characters
+        //difference must not be 32 for the case sensitivity to not matter
        if(s1[i] > s2[i] && abs(s1[i] - s2[i]) != 32){
                 return 1;
        }
+        //check if string 2 is greater than string 1
         if(s2[i] > s1[i] && abs(s1[i] - s2[i]) != 32){
                 return -1;
        }
    }
+   //Exiting the loops indicates the strings are equal
    return 0;
 }
 int main() {
