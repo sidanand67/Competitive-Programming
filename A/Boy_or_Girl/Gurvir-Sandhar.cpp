2aaae585023c500515098913e45f141fed0eb4d4
Gurvir <39442841+Gurvir-Sandhar@users.noreply.github.com>
2020-10-06 10:14:22 -0700

    Update Gurvir-Sandhar.cpp
    added comments

--- A/Boy_or_Girl/Gurvir-Sandhar.cpp
+++ A/Boy_or_Girl/Gurvir-Sandhar.cpp
@@ -8,10 +8,15 @@ int main(){
 	//get user input	
 	string input;
 	cin >> input;
-
+	
+	//array which each index represents an ascii char
 	string charList[256];
+	
+	//keeps track of number of distinct chars in the input string
 	int numDistinct;
 		
+	//for each char in the input string, check if it is in charList
+	//if not, then add it and increment numDistinct
 	for(int i = 0; i < input.length(); i++){ 
 		if(charList[input[i]] != "true"){
 			charList[input[i]] = "true";
@@ -19,6 +24,7 @@ int main(){
 		}		
 	}
 
+	//check if numDistinct is odd
 	if(numDistinct % 2 == 1) {
 		cout << "IGNORE HIM!" << endl;
 	}
