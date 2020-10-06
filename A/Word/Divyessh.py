eafb6b16908eab7a75c0d1b71671b86a8b261bc4
Divyessh Maheshwari <67703282+Divyessh@users.noreply.github.com>
2020-10-06 18:05:51 +0530

    Update Divyessh.py

--- A/Word/Divyessh.py
+++ A/Word/Divyessh.py
@@ -1,12 +1,15 @@
 def convert(s: str) -> str:
+    # converting the string to list 
     n = list(s)
     up = []
     down = []
+    # loop for seperation of string
     for i in n:
         if i.isupper() == True:
             up.append(i)
         elif i.islower() == True:
             down.append(i)
+    # checking case for conversion
     if len(up) > len(down):
         return s.upper()
     else:
