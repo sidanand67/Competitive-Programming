7ad9ed1f09e1997dee2b2f454b95d2ef69a5d03d
Subhra Sankha Sarma <51112668+imsubhra@users.noreply.github.com>
2020-10-07 17:02:37 +0530

    Update imsubhra.cpp

--- A/Stones_on_the_Table/imsubhra.cpp
+++ A/Stones_on_the_Table/imsubhra.cpp
@@ -2,17 +2,21 @@
 
 using namespace std;
 
-int main()
-{
-    int n,cnt=0,i;
-    char s[50];
-    scanf("%d",&n);
-    scanf("%s",s);
-    for(i=0; i<n-1; i++)
-    {
-        if(s[i]==s[i+1])
-            cnt++;
-    }
-    printf("%d\n",cnt);
-    return 0;
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	char s[255];
+	cin >> n; //no of stones in the table
+	for(int i = 0; i < n; i++){
+		cin >> s[i];
+	} //colours of stones
+	int cnt = 0; //variable to count minimum no of stones to take from the table so that any two neighbour stones had differemt colours
+	for(int i = 0; i < n-1; i++){
+		if(s[i] == s[i+1]) {
+			cnt++; // if adjacent colours are same then increment the cnt variable
+		}
+	}
+	cout << cnt;
+	return 0;
 }
