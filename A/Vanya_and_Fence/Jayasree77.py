ca98fe618e77abb045674d70d4f7b15bdc93a186
Jayasree77 <kjsree1998@gmail.com>
2020-10-08 19:41:42 +0530

    Vanya_and_Fence in python

--- A/Vanya_and_Fence/Jayasree77.py
+++ A/Vanya_and_Fence/Jayasree77.py
@@ -1,7 +1,7 @@
 n, h = map(int,input().split())
 a = list(map(int,input().split()))
 width = 0
-//If person's height is greater than fence width increases by 2 or else width increases by 1
+#If person's height is greater than fence width increases by 2 or else width increases by 1
 for i in range(0,len(a)):
   if(a[i]>h):
     width+=2
