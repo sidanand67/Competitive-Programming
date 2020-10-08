07d93e23e6af87fa9e07706fa00abc2336b11672
Ashish Burdak <45838960+ashishburdak@users.noreply.github.com>
2020-10-08 18:52:14 +0530

    added comments

--- A/Police_Recruits/ashishburdak.cpp
+++ A/Police_Recruits/ashishburdak.cpp
@@ -14,23 +14,26 @@ int main()
 {
     int n, police=0, crime=0;
     cin>>n;
+    
     int a[n];
     for(int i=0;i<n;++i)
     cin>>a[i];
+    
     for(int i=0;i<n;++i)
     {
-        if(a[i]==-1)
+        if(a[i]==-1) // crime has occurred
         {
-            if(police > 0)
-                police--;
+            if(police > 0) // police available to treat crime
+                police--; // one police officer can treat only one crime so reducing no. of available police officer
             else
-                crime++;
+                crime++; // no polie officer available, so this crime goes untreated
         }
         else
         {
-            police += a[i];
+            police += a[i]; //add police officers recruited
         }
     }
     cout<<crime<<endl;
+    
     return 0;
 }
