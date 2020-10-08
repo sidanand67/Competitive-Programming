3ba60ed6f1d9b239e819494a6443075b1d962ffe
Screamer <bratwurstbestand@web.de>
2020-10-08 17:01:16 +0200

    explanation

--- B/Sum_of_Digits/DragonBaronDev.java
+++ B/Sum_of_Digits/DragonBaronDev.java
@@ -6,18 +6,18 @@ public class DragonBaronDev {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String n = sc.nextLine();
-        int lenght = n.length();
-        while(lenght > 1){
-            int r = 0;
-            char[] stringToCharArr = n.toCharArray();//"11" -> "1" "1"
+        String n = sc.nextLine(); //Get the users input and store it as string 
+        int lenght = n.length(); // Stores the strings length
+        while(lenght > 1){ //As long as the strings size is smaller than 1, meaning smaller than 10
+            int r = 0; //The resulting number
+            char[] stringToCharArr = n.toCharArray();// Get every letter = number and store it inside the array
             for (char c : stringToCharArr) {
-                r+= Character.getNumericValue(c); //1+1 = 2
-                n = Integer.toString(r);
+                r+= Character.getNumericValue(c); //Add all numbers together and store it in r
+                n = Integer.toString(r);//Create a new string using the result
             }
-            lenght = n.length();
+            lenght = n.length(); //Assign new length
         }
-        System.out.println(n);
+        System.out.println(n); //if its done return the vaulue
         sc.close();
     }
 }
