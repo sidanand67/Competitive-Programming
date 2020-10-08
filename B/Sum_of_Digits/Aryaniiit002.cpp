5ea75c6f7752606ac6cc516a281f27e98e644d74
Aryan Bindal <55039523+aryaniiit002@users.noreply.github.com>
2020-10-08 15:38:10 +0530

    Update and rename Sum of Digits.cpp to Aryaniiit002.cpp

--- B/Sum_of_Digits/Aryaniiit002.cpp
+++ B/Sum_of_Digits/Aryaniiit002.cpp
@@ -14,12 +14,16 @@ int main() {
 	 cin>>s;
 	 int counter=0;
 	 int len=s.length();
+	// Loop will continue as long as length is more than 1 digit,or the condition is true until len is more than 1
 	while(len>1){
 		int sum=0;
 		for(int i=0;i<len;i++){
+			// add all the digits to sum
 			sum+=s[i]-'0';
 		}
+		// update the string that we are working on
 		 s=to_string(sum);
+		// Change its length
          len=s.length();
          ++counter;
 	 }
