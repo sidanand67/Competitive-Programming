bacf681f2ceeeb108c4713e8b08e306c8830d3a9
Aryan Bindal <55039523+aryaniiit002@users.noreply.github.com>
2020-10-08 08:19:10 +0530

    Add files via upload

---
+++ B/Sum_of_Digits/Sum of Digits.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+//problem link:http://codeforces.com/contest/102/problem/B
+// Author      : Aryan Bindal
+// Version     :
+//status:accepted
+//============================================================================
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	 string s;
+	 cin>>s;
+	 int counter=0;
+	 int len=s.length();
+	while(len>1){
+		int sum=0;
+		for(int i=0;i<len;i++){
+			sum+=s[i]-'0';
+		}
+		 s=to_string(sum);
+         len=s.length();
+         ++counter;
+	 }
+	 cout<<counter;
+	return 0;
+}
