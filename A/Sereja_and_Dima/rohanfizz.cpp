39959bbbdaa88fa14dc0c9e268fa4ee6b8adb0c5
Rohan Sharma <50175798+Rohanfizz@users.noreply.github.com>
2020-10-09 15:28:45 +0530

    Update rohanfizz.cpp

--- A/Sereja_and_Dima/rohanfizz.cpp
+++ A/Sereja_and_Dima/rohanfizz.cpp
@@ -24,12 +24,17 @@ int main(){
     int n; cin>>n;
     vi arr(n,0);
     for(int i = 0;i<n;i++) cin>>arr[i];
+       
 
-    int l = 0; int r = n-1;
-    int sereja = 0; int dima = 0; bool isSerejaTurn = true;
+
+
+
+    int l = 0; int r = n-1;                     //using two pointer approach
+    int sereja = 0; int dima = 0;               //sereja and dima's answer so far
+    bool isSerejaTurn = true;                   //will keep track of whose turn is it
     while(l<=r){
-        if(isSerejaTurn) sereja+= arr[l]>arr[r]?arr[l++]:arr[r--];
-        else dima+= arr[l]>arr[r]?arr[l++]:arr[r--];
+        if(isSerejaTurn) sereja+= arr[l]>arr[r]?arr[l++]:arr[r--];      // Sereja's turn- sereja will pick the largest of arr[l],arr[r]
+        else dima+= arr[l]>arr[r]?arr[l++]:arr[r--];                    // Dima's turn- sereja will pick the largest of arr[l],arr[r]
         isSerejaTurn = !isSerejaTurn;
     }
     cout<<sereja<<" "<<dima<<endl;
