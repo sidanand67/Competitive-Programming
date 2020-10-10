e2e1d73221af07a82d60ce8a605e05c9be84ad5f
Manan Jain <manansj01@gmail.com>
2020-10-10 09:58:42 +0530

    Changes updated

--- B/Presidential's_Office/MananOO12.cpp
+++ B/Presidential's_Office/MananOO12.cpp
@@ -1,46 +1,56 @@
 #include<bits/stdc++.h>
-#define ll long long
+#define twolong long long
 using namespace std;
 int main()
 {
-    ll i,j,n,cnt,m;
-    char s[101][101],pc;
-    while(cin>>n>>m>>pc){
-        for(i=0;i<n;i++){
-            for(j=0;j<m;j++){
-                cin>>s[i][j];
+    twolong i,j,length,count,width;
+    char office[101][101],color;
+    while(cin>>length>>width>>color)
+        {
+        for(i=0;i<length;i++)
+        {
+            for(j=0;j<width;j++)
+            {
+                cin>>office[i][j];
             }
         }
-        cnt=0;
+        count=0;
         bool mark[100];
         for(i='A';i<='Z';i++)
             mark[i]=0;
 
-        mark[pc]=1;
+        mark[color]=1;
         mark['.']=1;
-        for(i=0;i<n;i++){
-            for(j=0;j<m;j++){
-                if(s[i][j]==pc){
-                    if(mark[s[i][j+1]]==0 && j+1<m){
-                            cnt++;
-                            mark[s[i][j+1]]=1;
+        for(i=0;i<length;i++)
+            {
+            for(j=0;j<width;j++)
+            {
+                if(office[i][j]==color)
+                {
+                    if(mark[office[i][j+1]]==0 && j+1<width)
+                    {
+                            count++;
+                            mark[office[i][j+1]]=1;
                     }
-                    if(mark[s[i][j-1]]==0 && j-1>=0){
-                            cnt++;
-                            mark[s[i][j-1]]=1;                           
+                    if(mark[office[i][j-1]]==0 && j-1>=0)
+                    {
+                            count++;
+                            mark[office[i][j-1]]=1;
                     }
-                    if(mark[s[i+1][j]]==0 && i+1<n){
-                            cnt++;
-                            mark[s[i+1][j]]=1;
+                    if(mark[office[i+1][j]]==0 && i+1<length)
+                    {
+                            count++;
+                            mark[office[i+1][j]]=1;
                     }
-                    if(mark[s[i-1][j]]==0 && i-1>=0){
-                            cnt++;
-                            mark[s[i-1][j]]=1;
+                    if(mark[office[i-1][j]]==0 && i-1>=0)
+                    {
+                            count++;
+                            mark[office[i-1][j]]=1;
                     }
                 }
             }
         }
-        cout<<cnt<<endl;
+        cout<<count<<endl;
     }
 
     return 0;
