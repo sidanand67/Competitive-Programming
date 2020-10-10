9b1ebc17ac7933f5136a8537c551418793f77819
Chaitanya Mittal <chtnnh@gmail.com>
2020-10-10 12:19:57 +0400

    Add comments and rename variables in Vanya_and_Fence/chtnnh.c

--- A/Vanya_and_Fence/chtnnh.c
+++ A/Vanya_and_Fence/chtnnh.c
@@ -2,13 +2,15 @@
 
 
 int main(void) {
-	int n, h;
-	scanf("%d%d", &n, &h);
+	int numberOfFriends, heightOfFence;
+	scanf("%d%d", &numberOfFriends, &heightOfFence);
 	int arr[n], width = 0;
-	for (int i= 0; i < n; i++) {
+	for (int i= 0; i < numberOfFriends; i++) {
 		scanf("%d", &arr[i]);
-		if (arr[i] > h)
+		// if height is greater than height of fence widhth increased by 2
+		if (arr[i] > heightOfFence)
 			width += 2;
+		// else width increased by 1
 		else
 			width++;
 	}
