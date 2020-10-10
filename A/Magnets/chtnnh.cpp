3049fabc289e539fff07cace686a46ab010a6f04
Chaitanya Mittal <chtnnh@gmail.com>
2020-10-10 12:49:30 +0400

    Add comments to Magnets/chtnnh.cpp

--- A/Magnets/chtnnh.cpp
+++ A/Magnets/chtnnh.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 
 int main() {
+	// input number of magnets
 	int n;
 	cin >> n;
+	// init count as 1 because minimum number of groups is 1
 	int tracker = -1, count = 1, arr[n];
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
+		// if they are different, increase count and change tracker
 		if (arr[i] == 10 && tracker == 0) {
 			count++;
 			tracker = 1;
@@ -16,6 +19,7 @@ int main() {
 			count ++;
 			tracker = 0;
 		}
+		// initially set tracker value 
 		else if (tracker == -1 && arr[i] == 1) {
 			tracker = 0;
 		}
