01169168795ce117279233a5bf06e4738e3e98b3
Remya Krishnan <krishremya99@gmail.com>
2020-10-11 17:16:51 +0530

    Word Capitalization in c

---
+++ A/Word_Capitalization/krishremya.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   char str[1000];
+   //accepting string from user
+   scanf("%s",str);
+   int i=0;
+   if(str[i]>='a'&&str[i]<='z')
+   {
+       str[i]=str[i]-32; //converting small letters if any into capital letter
+   }
+   //printing the string
+   printf("%s",str);
+   return 0;
+}
