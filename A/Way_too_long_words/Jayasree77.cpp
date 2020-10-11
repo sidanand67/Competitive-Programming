0bafdd94b13e28461f11f5eb19d48bfa8d854b69
Kurmannagari <Jayasree.Kurmannagari@commscope.com>
2020-10-11 10:55:17 +0530

    Way too long words in C++

---
+++ A/Way_too_long_words/Jayasree77.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;cin>>n;
+    string s;
+    for(int i=0;i<n;i++){
+    cin>>s;
+    int size = s.size();
+    if(size>10)
+    cout<<s[0]+to_string(size-2)+s[size-1]<<endl;
+    else
+    cout<<s<<endl;
+    }
+    return 0;
+}
