93a4705bfb6631c89d38a057038dcfbdfe5cb964
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-10-12 17:59:27 +0530

    Update and rename soldier_and_bananas.py to fazeel.py

---
+++ A/Soldier_and_Bananas/fazeel.py
@@ -0,0 +1,13 @@
+values = list(int(x) for x in input().split())
+
+k = values[0]    # cost of first banana
+n = values[1]    # initial number of dollars the soldier has 
+w = values[2]    # number of bananas he has 
+
+
+price = ((k*w*(w+1))//2)
+
+if(price <= n):   # If price required to purchase w bananas is < number of dollars he possess 
+  print(0)
+else:
+  print(price-n)  # else return the difference
