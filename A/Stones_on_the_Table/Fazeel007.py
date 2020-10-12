2910f3e50d38182311bdb194e2118ab5ea1cef4a
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-10-12 18:37:38 +0530

    Update Fazeel007.py

--- A/Stones_on_the_Table/Fazeel007.py
+++ A/Stones_on_the_Table/Fazeel007.py
@@ -1,9 +1,9 @@
-n = int(input())
-str = input()
+n = int(input())    # number of stones on the table 
+str = input()       # string of colors in a row where RRB represents Red Red and Blue
  
 cnt = 0
 for i in range(1, n):
-  if (str[i] == str[i-1]):
+  if (str[i] == str[i-1]):    # checking if the ith color is same to its previous if yes then we need to remove one of them 
     cnt += 1
     
 print(cnt)
