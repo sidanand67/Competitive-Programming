20d06df98356c1c53e0c6b3894b7ec7498f81c16
premk0018 <72334563+premk0018@users.noreply.github.com>
2020-10-13 17:38:42 +0530

    added comments to the solution

--- A/Raising_Bacteria/premk0018.cpp
+++ A/Raising_Bacteria/premk0018.cpp
@@ -4,19 +4,25 @@ using namespace std;
 int main(){
     long long int n,t,m,r=0;
     cin>>n;
+    //a bacteria splits into ao this gives an idea two use bitmasking
+    //initially the box is empty
+    //we need to return minimum bacteria's that should be added to box so that at some day the box will contain exactly n no. of bacteria's
     t=n;
-    while(t>0){
-        m=1;
-        r++;
+    //r stores the min. no. of bacteria's that we should add
+    while(t>0){ // while the box does not contains exact n no. of bacteria's we performs following statements
+        m=1; // for each time we add one bacteria therefore m=1
+        r++; // since a bacteria was added so we increment r
         while(m<t){
-            m<<=1;
+            m<<=1;  // this loop finally gives us the nearest no. to t (current counter for bacteria's which are still required) which is pow of 2
         }
+        //m will either be equal to t if t is a pow of 2 or it will be a pow of 2 greater than t
         if(t!=m){
-           m>>=1;
+           m>>=1;  // if m is greater than t we makes it smaller
         }
         t=n-m;
-        n=t;
+        n=t; 
+        // if still some bacteria's left we runs the loop again
     }
-    cout<<r<<endl;
+    cout<<r<<endl; // finally r will have the min. no.of bacteria added to reach n
     return 0;
 }
