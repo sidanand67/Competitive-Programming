b0250e15a324c7f301ae1f6512bce37dd273cbb5
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2020-10-17 12:31:56 +0530

    add comments to problem Sort_the_Array

--- B/Sort_the_Array/isuru.cpp
+++ B/Sort_the_Array/isuru.cpp
@@ -1,31 +1,34 @@
 #include <stdio.h>
 
 int main(){
-    int n;
-    scanf("%d",&n);
+    int n;  
+    scanf("%d",&n);   //get the number of integers
     int arr[n];
-    for(int i=0 ; i<n ; i++){
-        scanf("%d",&arr[i]);
+    for(int i=0 ; i<n ; i++){    //loop for get the integers
+        scanf("%d",&arr[i]);     //get the intergers
     }
-    int first = 0;
+    int first = 0;       
     int last;
     int done = 0;
-    for(int i=0 ; i<n-1 ; i++){
-        if(arr[i]<=arr[i+1]){
-            if(!last){
-                first = i+1;
+    for(int i=0 ; i<n-1 ; i++){   //loop the number list
+        if(arr[i]<=arr[i+1]){     //check if this element is lower than the next element in the list
+            if(!last){            //check if theres already a change has happend
+                first = i+1;     
             }
+            //if theres aready a change should happen
             else{
-                if(arr[first-1]<=arr[last] && arr[first]<=arr[i+1]){
-                    done = 1;
+                if(arr[first-1]<=arr[last] && arr[first]<=arr[i+1]){  //rearrange the list
+                    done = 1;      //to check if there's one change or more than one change
                 }
+                //if there's two changes possible
                 else{
-                    done = 2;
-                    break;
+                    done = 2;    //to check if there's one change or more than one change
+                    break;       //breakbecause we dont have to look more posibilities it should be ended and print "no"
                 }
             }
             
         }
+        //if a change should happen
         else{
             if(done == 0){
                 last = i+1;
@@ -37,7 +40,7 @@ int main(){
             
         }
     }
-    if(done ==2){
+    if(done ==2){     //done=2 means thereshould more changes than one
         printf("no");
     }
     else{
