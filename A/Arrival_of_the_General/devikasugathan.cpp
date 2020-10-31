5f4bce3e4b8bcdca0db2064a0db02874e59ba7e7
devikasugathan <devikasugathan007@gmail.com>
2020-10-31 07:01:25 +0530

    Added Arrival_of_the_General

--- A/Arrival_of_the_General/devikasugathan.cpp
+++ A/Arrival_of_the_General/devikasugathan.cpp
@@ -1,10 +1,10 @@
 # include<bits/stdc++.h>
 using namespace std;
-int  n,k,c;
+int  n,k,c; // initial value set to zero, k and c to store comparing values
 int main()
 {
-   cin>>n;  // number of soldiers
-   int a[n];
+   cin>>n;  // the number of soldiers
+   int a[n];  //
    for(int i=0;i<n;i++)
    {
        cin>>a[i];
